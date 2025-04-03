ee8ed3ca61969e4f8761cb24c42c1a18079bfc41
evalstate <1936278+evalstate@users.noreply.github.com>
2025-04-03 08:51:02 +0100

    update example

--- examples/workflows/parallel.py
+++ examples/workflows/parallel.py
@@ -3,34 +3,15 @@ Parallel Workflow showing Fan Out and Fan In agents, using different models
 """
 
 import asyncio
+from pathlib import Path
 
 from mcp_agent.core.fastagent import FastAgent
+from mcp_agent.core.prompt import Prompt
 
 # Create the application
 fast = FastAgent(
     "Parallel Workflow",
 )
-SHORT_STORY = """
-The Battle of Glimmerwood
-
-In the heart of Glimmerwood, a mystical forest knowed for its radiant trees, a small village thrived. 
-The villagers, who were live peacefully, shared their home with the forest's magical creatures, 
-especially the Glimmerfoxes whose fur shimmer like moonlight.
-
-One fateful evening, the peace was shaterred when the infamous Dark Marauders attack. 
-Lead by the cunning Captain Thorn, the bandits aim to steal the precious Glimmerstones which was believed to grant immortality.
-
-Amidst the choas, a young girl named Elara stood her ground, she rallied the villagers and devised a clever plan.
-Using the forests natural defenses they lured the marauders into a trap. 
-As the bandits aproached the village square, a herd of Glimmerfoxes emerged, blinding them with their dazzling light, 
-the villagers seized the opportunity to captured the invaders.
-
-Elara's bravery was celebrated and she was hailed as the "Guardian of Glimmerwood". 
-The Glimmerstones were secured in a hidden grove protected by an ancient spell.
-
-However, not all was as it seemed. The Glimmerstones true power was never confirm, 
-and whispers of a hidden agenda linger among the villagers.
-"""
 
 
 @fast.agent(
@@ -67,13 +48,11 @@ and whispers of a hidden agenda linger among the villagers.
     name="parallel",
 )
 async def main() -> None:
-    # Use the app's context manager
     async with fast.run() as agent:
-        await agent.parallel.send(f"student short story submission: {SHORT_STORY}")
-
-        # follow-on prompt to task agent
-        await agent.style_enforcer.prompt(default="STOP")
+        await agent.parallel.send(
+            Prompt.user("Student short story submission", Path("short_story.txt"))
+        )
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main())  # type: ignore
